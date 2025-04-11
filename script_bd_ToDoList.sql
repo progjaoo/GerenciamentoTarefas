3e7259e37fcc1273d76eacf69ae3ee6e1fd17b93
João Marcos dos Santos Valente <100044031+progjaoo@users.noreply.github.com>
2025-04-10 23:41:09 -0300

    Add files via upload

--- script_bd_ToDoList.sql
+++ script_bd_ToDoList.sql
@@ -1,9 +1,11 @@
 CREATE DATABASE GerenciamentoTarefas
 
+USE GerenciamentoTarefas
+
 CREATE TABLE Tarefas (
     Id INT IDENTITY(1,1) PRIMARY KEY,
-    Titulo NVARCHAR(255) NOT NULL,
-    Descricao NVARCHAR(MAX) NULL,
+    Titulo NVARCHAR(100) NOT NULL,
+    Descricao NVARCHAR(300) NULL,
     DataCriacao DATETIME NOT NULL DEFAULT GETDATE(),
     DataConclusao DATETIME NULL,
     Status INT NOT NULL
